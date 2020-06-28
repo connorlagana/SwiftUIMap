ea758ef7d28000b914bb0b7728f8d8c55e8f33ca
Connor Lagana <connor.lagana@gmail.com>
2020-06-28 13:41:18 -0400

    feel free

--- SwiftUIMap/Directions/DirectionsController.swift
+++ SwiftUIMap/Directions/DirectionsController.swift
@@ -10,149 +10,176 @@ import UIKit
 import LBTATools
 import MapKit
 import SwiftUI
-
+import JGProgressHUD
 
 class DirectionsController: UIViewController, MKMapViewDelegate {
     
     let mapView = MKMapView()
-    let navBar = UIView(backgroundColor: .init(red: 120/255, green: 220/255, blue: 165/255, alpha: 1))
+    let navBar = UIView(backgroundColor: #colorLiteral(red: 0.1948848367, green: 0.7962132096, blue: 0.9489386678, alpha: 1))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         view.addSubview(mapView)
-//        mapView.fillSuperview()
         
-        navigationController?.navigationBar.isHidden = true
-        
+        // setup region for SF
         setupRegionForMap()
+        
         setupNavBarUI()
+        
         mapView.anchor(top: navBar.bottomAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
         
         mapView.showsUserLocation = true
         
-        setupStartEnd()
-        requestDirections()
+//        setupStartEndDummyAnnotations()
+//        requestForDirections()
+        
     }
     
-    fileprivate func setupStartEnd() {
-        let startAnno = MKPointAnnotation()
-        startAnno.coordinate = .init(latitude: 37.7666, longitude: -122.42729)
-        startAnno.title = "Start"
+    fileprivate func setupStartEndDummyAnnotations() {
+        let startAnnotation = MKPointAnnotation()
+        startAnnotation.coordinate = .init(latitude: 37.7666, longitude: -122.427290)
+        startAnnotation.title = "Start"
         
-        let endAnno = MKPointAnnotation()
-        endAnno.coordinate = .init(latitude: 37.3666, longitude: -122.02729)
-        endAnno.title = "End"
+        let endAnnotation = MKPointAnnotation()
+        endAnnotation.coordinate = .init(latitude: 37.331352, longitude: -122.030331)
+        endAnnotation.title = "End"
         
-        mapView.addAnnotation(startAnno)
-        mapView.addAnnotation(endAnno)
+        mapView.addAnnotation(startAnnotation)
+        mapView.addAnnotation(endAnnotation)
         
         mapView.showAnnotations(mapView.annotations, animated: true)
     }
     
-    fileprivate func requestDirections() {
+    fileprivate func requestForDirections() {
         let request = MKDirections.Request()
+        request.source = startMapItem
+        request.destination = endMapItem
         
-        let startPM = MKPlacemark(coordinate: .init(latitude: 37.7666, longitude: -122.42729))
-        let endPM = MKPlacemark(coordinate: .init(latitude: 37.3666, longitude: -122.02729))
-        
-        request.source = .init(placemark: startPM)
-        request.destination = .init(placemark: endPM)
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Routing..."
+        hud.show(in: view)
         
         let directions = MKDirections(request: request)
         directions.calculate { (resp, err) in
+            hud.dismiss()
+            
             if let err = err {
-                print("Failed to find routing info: ", err)
+                print("Failed to find routing info:", err)
                 return
             }
-            //success
-            print("directions! fuck yeah!")
-            //getting first route possible
-            guard let route = resp?.routes.first else { return }
             
-//            route.advisoryNotices
-//            route.distance
-//            route.expectedTravelTime
-//            route.polyline
-            
-            self.mapView.addOverlay(route.polyline)
-            //polyline still has to render after adding overlay
+            // success
+            print("Found my directions/routing....")
+            resp?.routes.forEach({ (route) in
+                self.mapView.addOverlay(route.polyline)
+            })
         }
-        
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        let polyLineRenderer = MKPolylineRenderer(overlay: overlay)
-        polyLineRenderer.strokeColor = .init(red: 120/255, green: 220/255, blue: 165/255, alpha: 1)
-        polyLineRenderer.lineWidth = 5
-        return polyLineRenderer
+        let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+        polylineRenderer.strokeColor = #colorLiteral(red: 0.1948848367, green: 0.7962132096, blue: 0.9489386678, alpha: 1)
+        polylineRenderer.lineWidth = 5
+        return polylineRenderer
     }
     
     let startTextField = IndentedTextField(padding: 12, cornerRadius: 5)
     let endTextField = IndentedTextField(padding: 12, cornerRadius: 5)
     
     fileprivate func setupNavBarUI() {
-        
         view.addSubview(navBar)
         navBar.setupShadow(opacity: 0.5, radius: 5)
         navBar.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.topAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: -120, right: 0))
         
         startTextField.attributedPlaceholder = .init(string: "Start", attributes: [.foregroundColor: UIColor.init(white: 1, alpha: 0.7)])
+        
         endTextField.attributedPlaceholder = .init(string: "End", attributes: [.foregroundColor: UIColor.init(white: 1, alpha: 0.7)])
         
         [startTextField, endTextField].forEach { (tf) in
             tf.backgroundColor = .init(white: 1, alpha: 0.3)
+            tf.textColor = .white
         }
         
         let containerView = UIView(backgroundColor: .clear)
+        navBar.addSubview(containerView)
+        containerView.fillSuperviewSafeAreaLayoutGuide()
+        
         
         let startIcon = UIImageView(image: #imageLiteral(resourceName: "start_location_circles"), contentMode: .scaleAspectFit)
-        let endIcon = UIImageView(image: #imageLiteral(resourceName: "annotation_icon").withRenderingMode(.alwaysTemplate), contentMode: .scaleAspectFit)
-        
         startIcon.constrainWidth(20)
+        
+        let endIcon = UIImageView(image: #imageLiteral(resourceName: "annotation_icon").withRenderingMode(.alwaysTemplate), contentMode: .scaleAspectFit)
         endIcon.constrainWidth(20)
         endIcon.tintColor = .white
         
-        navBar.addSubview(containerView)
-        containerView.fillSuperviewSafeAreaLayoutGuide()
-        containerView
-            .stack(
-                containerView.hstack(startIcon, startTextField, spacing: 16),
-                containerView.hstack(endIcon, endTextField, spacing: 16),
-                spacing: 12,
-                distribution: .fillEqually
-                )
-            .withMargins(.init(top: 0, left: 12, bottom: 12, right: 12))
+        containerView.stack(
+            containerView.hstack(startIcon, startTextField, spacing: 16),
+            containerView.hstack(endIcon, endTextField, spacing: 16),
+                            spacing: 12,
+                            distribution: .fillEqually)
+            .withMargins(.init(top: 0, left: 16, bottom: 12, right: 16))
         
-        startTextField
-            .addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleChangeStartLocation)))
-        endTextField
-            .addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleChangeEndLocation)))
+        startTextField.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleChangeStartLocation)))
+        
+        endTextField.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleChangeEndLocation)))
+        
+        navigationController?.navigationBar.isHidden = true
     }
     
+    var startMapItem: MKMapItem?
+    var endMapItem: MKMapItem?
+    
     @objc fileprivate func handleChangeStartLocation() {
-        
         let vc = LocationSearchController()
-        
         vc.selectionHandler = { [weak self] mapItem in
             self?.startTextField.text = mapItem.name
+            
+            // add starting annotation and also show it in the map
+            self?.startMapItem = mapItem
+            self?.refreshMap()
         }
         
         navigationController?.pushViewController(vc, animated: true)
+    }
     
+    func refreshMap() {
+        // remove everything from map
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.removeOverlays(mapView.overlays)
+        
+        if let mapItem = startMapItem {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = mapItem.placemark.coordinate
+            annotation.title = mapItem.name
+            mapView.addAnnotation(annotation)
+        }
+        
+        if let mapItem = endMapItem {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = mapItem.placemark.coordinate
+            annotation.title = mapItem.name
+            mapView.addAnnotation(annotation)
+        }
+        
+        requestForDirections()
+        
+        mapView.showAnnotations(mapView.annotations, animated: false)
     }
     
     @objc fileprivate func handleChangeEndLocation() {
-        
         let vc = LocationSearchController()
-        
         vc.selectionHandler = { [weak self] mapItem in
-            
             self?.endTextField.text = mapItem.name
+            self?.endMapItem = mapItem
+            self?.refreshMap()
         }
         
         navigationController?.pushViewController(vc, animated: true)
+    }
     
+    @objc func handleBack() {
+        navigationController?.popViewController(animated: true)
     }
     
     fileprivate func setupRegionForMap() {
@@ -163,20 +190,24 @@ class DirectionsController: UIViewController, MKMapViewDelegate {
     }
 }
 
-
-
 struct DirectionsPreview: PreviewProvider {
+    
     static var previews: some View {
-        ContainerView().edgesIgnoringSafeArea(.all).environment(\.colorScheme, .light)
+        ContainerView().edgesIgnoringSafeArea(.all)
+//            .environment(\.colorScheme, .light)
     }
     
     struct ContainerView: UIViewControllerRepresentable {
+        
         func makeUIViewController(context: UIViewControllerRepresentableContext<DirectionsPreview.ContainerView>) -> UIViewController {
             return UINavigationController(rootViewController: DirectionsController())
         }
         
-        func updateUIViewController(_ uiViewController: DirectionsPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<DirectionsPreview.ContainerView> ) {
+        func updateUIViewController(_ uiViewController: DirectionsPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<DirectionsPreview.ContainerView>) {
             
         }
+        
     }
+    
 }
+
