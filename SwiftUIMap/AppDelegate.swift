19d49d0d70bb4a3a6efb6c82fa85894c726fd70c
Connor Lagana <connor.lagana@gmail.com>
2020-06-29 18:02:11 -0400

    api key

--- SwiftUIMap/AppDelegate.swift
+++ SwiftUIMap/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSPlacesClient.provideAPIKey("")
+        GMSPlacesClient.provideAPIKey("AIzaSyBWqllfqyRTITChZ2NOBZW9fNoDesSfHbg")
         
         return true
     }
